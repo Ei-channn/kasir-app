8b575db33ea0f4d120b8f0dbdf37bbad8325b32a
Ei_chann <rifqi070409@gmail.com>
2025-09-24 19:10:31 +0700

    bootstrap

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,7 @@
+@extends('template.master')
+@section('content')
+<div class="content">
+    <h2>Selamat Datang di Pos System</h2>
+    <p>Ini adalah halaman utama pengguna.</p>
+</div>
+@endsection
